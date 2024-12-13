3825360a5ac9ce78994df8ba80c3623ece374151
Oscar1999-L <oscar.ramirez990719@gmail.com>
2024-12-12 21:43:28 -0600

    Primer commit de Alura Hub

---
+++ ForoApi/src/test/java/alura/foro/ApiApplicattion/ForoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package alura.foro.ApiApplicattion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
